7144d6c10efc6050a4ae298191fcadeb9efae5f3
Samuel García Martínez <samuelgma@inditex.com>
2021-03-07 10:17:49 +0100

    refactor basic structure for the repo

--- packages/validate-schema-action/index.js
+++ packages/validate-schema-action/index.js
@@ -1,15 +1,34 @@
+const fs = require('fs')
 const core = require('@actions/core');
 const github = require('@actions/github');
+const glob = require('glob')
+const Ajv = require('ajv')
+
+const ajv = new Ajv.default({allErrors: true})
 
 try {
+  const subjectsPath = core.getInput('subjects-path');
+
+  const files = glob.sync(subjectsPath + '/**/*.json')
+    .map(schemaPath => {
+      console.log(`Validating schema definition: ${schemaPath}`);
+      return JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
+    })
+    .forEach(schema => ajv.addSchema(schema));
+  
+
+  /*
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  */
 } catch (error) {
   core.setFailed(error.message);
 }
