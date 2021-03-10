35d58124d67ad48b75d342d1f57144532d8f2213
Samuel Garcia Martinez <samuelgma@inditex.com>
2021-03-10 11:57:28 +0100

    uses async function for the validation action

--- packages/validate-schema-action/index.js
+++ packages/validate-schema-action/index.js
@@ -1,19 +1,23 @@
 const core = require('@actions/core');
-const validator = require('validator')
+const validator = require('./validator')
 
-try {
-  const result = validator.validate(core.getInput('subjects-path') + '/**/*.json')
+async function run() {
+  try {
+    const result = validator.validate(core.getInput('subjects-path') + '/**/*.json')
 
-  core.setOutput('parsed-schemas', result.parsedSchemas);
+    core.setOutput('parsed-schemas', result.parsedSchemas);
 
-  if (result.successfulSchemas.length) {
-    core.setOutput('successful-schemas', result.successfulSchemas);
+    if (result.successfulSchemas.length) {
+      core.setOutput('successful-schemas', result.successfulSchemas);
+    }
+
+    if (result.failedSchemas.length) {
+      core.setFailed("Some json schema validations have failed. See the logs for more information.");
+      core.setOutput('failed-schemas', result.failedSchemas);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  if (result.failedSchemas.length) {
-    core.setFailed("Some json schema validations have failed. See the logs for more information.");
-    core.setOutput('failed-schemas', result.failedSchemas);
-  }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
