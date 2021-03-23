85a27458e52b488151213866263cae4adfcc92d9
Samuel Garcia Martinez <samuelgma@inditex.com>
2021-03-23 12:20:07 +0100

    adds auto approval PRs

--- packages/schema-publisher-action/index.js
+++ packages/schema-publisher-action/index.js
@@ -50,7 +50,7 @@ async function run() {
       basePath: s3env.basePath
     }
 
-    console.log('Test message');
+    console.log('Test message2');
 
     await publisher.publish(pathExpr, s3config, core.getInput('owner'), core.getInput('force') == 'true');
   } catch (error) {
